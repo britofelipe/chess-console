2457104b3348c04900e24cf9401de682bac1f346
britofelipe <arrudabritofelipe@gmail.com>
2023-01-03 09:41:40 -0300

    Creating Position class

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using table;
+
+namespace chess_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position P;
+            P = new Position(3, 4);
+
+            Console.WriteLine("Position: " + P);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ chess-console/table/Position.cs
@@ -0,0 +1,21 @@
+﻿namespace table
+{
+    class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int line, int column)
+        {
+            Row = line;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Row
+                + ", "
+                + Column;
+        }
+    }
+}
