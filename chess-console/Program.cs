5d3b2646236ac5d5ed098a1503104268c2463535
britofelipe <arrudabritofelipe@gmail.com>
2023-01-05 10:03:15 -0300

    Creating ChessPosition

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -8,21 +8,26 @@ namespace chess_console
     {
         static void Main(string[] args)
         {
-            Board board = new Board(8, 8);
-            try
-            {
-                board.putPiece(new Tower(board, Color.Black), new Position(0, 0));
-                board.putPiece(new Tower(board, Color.Black), new Position(1, 3));
-                board.putPiece(new King(board, Color.Black), new Position(2, 4));
-                // board.putPiece(new Tower(board, Color.Black), new Position(0, 0));
-                board.putPiece(new Tower(board, Color.Black), new Position(0, 8));
+            //Board board = new Board(8, 8);
+            //try
+            //{
+            //    board.putPiece(new Tower(board, Color.Black), new Position(0, 0));
+            //    board.putPiece(new Tower(board, Color.Black), new Position(1, 3));
+            //    board.putPiece(new King(board, Color.Black), new Position(2, 4));
+            //    // board.putPiece(new Tower(board, Color.Black), new Position(0, 0));
+            //    board.putPiece(new Tower(board, Color.Black), new Position(0, 8));
 
-                Screen.printTable(board);
-            }
-            catch(BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            //    Screen.printTable(board);
+            //}
+            //catch(BoardException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            ChessPosition pos = new ChessPosition('a', 1);
+            Console.WriteLine(pos.ToString());
+            Console.WriteLine(pos.toPosition());
+
             Console.ReadLine();
         }
     }
---
+++ chess-console/chess/ChessPosition.cs
@@ -0,0 +1,26 @@
+﻿using board;
+
+namespace chess
+{
+    internal class ChessPosition
+    {
+        public char column { get; set; }
+
+        public int row { get; set; }
+        public ChessPosition(char column, int row)
+        {
+            this.column = column;
+            this.row = row;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - row, column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + column + row;
+        }
+    }
+}
